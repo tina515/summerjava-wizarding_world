6c0048b10f70f14831494ee4f6f1c1de8f42351e
Tina Behzad <Tina@Tinas-MacBook-Pro.local>
2017-07-28 13:03:38 +0430

    get and set houses name test

--- wizarding_world/src/entities/School.java
+++ wizarding_world/src/entities/School.java
@@ -20,12 +20,11 @@ public class School {
 		courseNames = null;
 		schoolPrinciple = "";
 	}
-	public School(String _name, int _numOfStudents, boolean _housed, ArrayList<String> _houseNames
-			,Vector<String> _studentNames, Vector<String> _professorNames,
-				ArrayList<String> _courseNames)
+	public School(String _name, ArrayList<String> _houseNames,ArrayList<String> _courseNames,boolean _housed
+			,Vector<String> _studentNames, Vector<String> _professorNames)
 	{
 		name = _name;
-		numOfStudents = _numOfStudents;
+		numOfStudents = 0;
 		housed = _housed;
 		houseNames = _houseNames;
 		studentNames = _studentNames;
@@ -46,8 +45,23 @@ public class School {
 		courseNames = _courseNames;
 		schoolPrinciple = _principle;
 	}	
+	public School(String _name, int _numOfStudents, boolean _housed, ArrayList<String> _houseNames
+			,Vector<String> _studentNames, Vector<String> _professorNames,
+				ArrayList<String> _courseNames)
+	{
+		name = _name;
+		numOfStudents = _numOfStudents;
+		housed = _housed;
+		houseNames = _houseNames;
+		studentNames = _studentNames;
+		professorNames = _professorNames;
+		courseNames = _courseNames;
+		schoolPrinciple = "";
+	}
 	public void setName(String _name) { name = _name;}
 	public String getName() { return name;}
 	public void setHoused(boolean _housed) {housed = _housed;}
 	public boolean getHoused() {return housed;}
+	public void setHouseNames(ArrayList<String> _houseNames) {houseNames = _houseNames;}
+	public ArrayList <String> getHouseNames() { return houseNames;}
 }
--- wizarding_world/src/entities/wizarding_world.java
+++ wizarding_world/src/entities/wizarding_world.java
@@ -1,13 +1,25 @@
 package entities;
+import java.util.*;
 
 public class wizarding_world {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		School hogwarts = new School("Hogwarts");
-		System.out.println(hogwarts.getName());
-		hogwarts.setName("sometging else");
-		System.out.println(hogwarts.getName());
+		ArrayList<String> houses = new ArrayList<String>();
+		houses.add("Gryffindor");
+		houses.add("Hufflepuff");
+		houses.add("Ravenclaw");
+		houses.add("Slytherin");
+		School hogwarts = new School("Hogwarts", houses, null, true, null, null);
+		System.out.println(hogwarts.getHouseNames());
+		ArrayList<String> _houses = new ArrayList<String>();
+		_houses.add("Gryffindor");
+		_houses.add("Hufflepuff");
+		_houses.add("Ravenclaw");
+		_houses.add("Slytherin");
+		School hogwarts2 = new School("Hogwarts");
+		hogwarts2.setHouseNames(_houses);
+		System.out.println(hogwarts2.getHouseNames());
 	}
 
 }
